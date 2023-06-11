72473c0e97478d973b870a06c7279c6624f07f0e
LuyandaDingindlela101 <luyandadingindlela@gmail.com>
2023-06-11 18:34:10 +0200

    completed feed component, moving on to sidebar component

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,4 +1,5 @@
 import "./feed.css"
+import Post from "../post/Post"
 import Share from "../share/Share"
 
 export default function Feed() {
@@ -6,6 +7,20 @@ export default function Feed() {
 		<div className="feed">
 			<div className="feed-container">
 				<Share />
+				<Post />
+				<Post />
+				<Post />
+				<Post />
+				<Post />
+				<Post />
+				<Post />
+				<Post />
+				<Post />
+				<Post />
+				<Post />
+				<Post />
+				<Post />
+				<Post />
 			</div>
 		</div>
 	)
