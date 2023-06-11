f22e0d8cdc720a0936dc4502bc5013db49ef3bd5
LuyandaDingindlela101 <luyandadingindlela@gmail.com>
2023-06-11 19:13:42 +0200

    completed rightbar component, gonna be moving onto populating dummy data

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,9 +1,103 @@
 import "./rightbar.css"
+import { CardGiftcard } from "@mui/icons-material"
 
 export default function Rightbar() {
 	return (
 		<div className="rightbar">
-			
+			<div className="rightbar-container">
+				<div className="birthday-container">
+					<CardGiftcard className="birthday-icon"/>
+					<span className="birthday-text"><b>Pola Foster</b> & <b>3 other friends</b> have birthdays today</span>
+				</div>
+				<div className="rightbar-ad"></div>
+				<h4 className="rightbar-title">Online frendz</h4>
+				<ul className="rightbar-friend-list">
+					<li className="rightbar-friend">
+						<div className="rightbar-profile-container">
+							<div className="rightbar-profile-image"></div>
+							<span className="rightbar-profile-online"></span>
+						</div>
+						<span className="rightbar-username">Hilly</span>
+					</li>
+					<li className="rightbar-friend">
+						<div className="rightbar-profile-container">
+							<div className="rightbar-profile-image"></div>
+							<span className="rightbar-profile-online"></span>
+						</div>
+						<span className="rightbar-username">Villy</span>
+					</li>
+					<li className="rightbar-friend">
+						<div className="rightbar-profile-container">
+							<div className="rightbar-profile-image"></div>
+							<span className="rightbar-profile-online"></span>
+						</div>
+						<span className="rightbar-username">Aj</span>
+					</li>
+					<li className="rightbar-friend">
+						<div className="rightbar-profile-container">
+							<div className="rightbar-profile-image"></div>
+							<span className="rightbar-profile-online"></span>
+						</div>
+						<span className="rightbar-username">Kt</span>
+					</li>
+					<li className="rightbar-friend">
+						<div className="rightbar-profile-container">
+							<div className="rightbar-profile-image"></div>
+							<span className="rightbar-profile-online"></span>
+						</div>
+						<span className="rightbar-username">Hilly</span>
+					</li>
+					<li className="rightbar-friend">
+						<div className="rightbar-profile-container">
+							<div className="rightbar-profile-image"></div>
+							<span className="rightbar-profile-online"></span>
+						</div>
+						<span className="rightbar-username">Villy</span>
+					</li>
+					<li className="rightbar-friend">
+						<div className="rightbar-profile-container">
+							<div className="rightbar-profile-image"></div>
+							<span className="rightbar-profile-online"></span>
+						</div>
+						<span className="rightbar-username">Aj</span>
+					</li>
+					<li className="rightbar-friend">
+						<div className="rightbar-profile-container">
+							<div className="rightbar-profile-image"></div>
+							<span className="rightbar-profile-online"></span>
+						</div>
+						<span className="rightbar-username">Kt</span>
+					</li>
+					<li className="rightbar-friend">
+						<div className="rightbar-profile-container">
+							<div className="rightbar-profile-image"></div>
+							<span className="rightbar-profile-online"></span>
+						</div>
+						<span className="rightbar-username">Hilly</span>
+					</li>
+					<li className="rightbar-friend">
+						<div className="rightbar-profile-container">
+							<div className="rightbar-profile-image"></div>
+							<span className="rightbar-profile-online"></span>
+						</div>
+						<span className="rightbar-username">Villy</span>
+					</li>
+					<li className="rightbar-friend">
+						<div className="rightbar-profile-container">
+							<div className="rightbar-profile-image"></div>
+							<span className="rightbar-profile-online"></span>
+						</div>
+						<span className="rightbar-username">Aj</span>
+					</li>
+					<li className="rightbar-friend">
+						<div className="rightbar-profile-container">
+							<div className="rightbar-profile-image"></div>
+							<span className="rightbar-profile-online"></span>
+						</div>
+						<span className="rightbar-username">Kt</span>
+					</li>
+				</ul>
+			</div>
 		</div>
 	)
 }
